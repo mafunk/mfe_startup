78b8eece260083be054d1c1e0b70f0a7823e68fc
funkstyr <funkstyr@gmail.com>
2021-01-24 16:55:43 -0500

    init marketing

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+function mount(element) {
+  ReactDOM.render(<App />, element);
+}
+
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+  if (devRoot) mount(devRoot);
+}
+
+export { mount };
